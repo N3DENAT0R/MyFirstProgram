4fe3eb1d2812cecb2adc40d6c552cb123c707123
N3DENAT0R <nedelcho_dinev@abv.bg>
2015-01-20 21:16:10 +0200

    MyFirstProgram
    Hi guys thats my first C# program im a newbie so you can give me tips if
    you want.

---
+++ MyFirstProgram/MyFirstProgram/MyFirstProgram.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class TheProgram
+    {
+
+        static void Main()
+        {
+            Console.WriteLine("Press the ENTER key to close the window.");
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProgram
+{
+    class TheProgram
+    {
+
+        static void Main()
+        {
+            Console.WriteLine("Press the ENTER key to close the window.");
+            Console.ReadLine();
+        }
+
+    }
+}
